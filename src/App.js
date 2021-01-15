bf92b3a27d7e8b54ba49152e6ece733eccefd76b
ndfsa <sergiomarzana1@gmail.com>
2021-01-14 23:18:56 -0400

    Using hooks instead of classes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const States = ['N', 'x', 'o']
+
+function Switch() {
+    const [token, setToken] = useState(0)
+    return (
+        <div>
+            <p>state {States[token]}</p>
+            <button onClick={() => setToken((token + 1) % 3)}></button>
+        </div>
+    )
 }
 
-export default App;
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <Switch />
+            </header>
+        </div>
+    )
+}
+
+export default App
